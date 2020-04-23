a94da71678a5fbb5578042983c8299b772e2c750
Luciano Almeida Pimenta <pis0417@supero.com.br>
2020-04-23 10:08:12 -0300

    Adicionado imagens e descrições da lista

--- lib/pages/card.dart
+++ lib/pages/card.dart
@@ -6,11 +6,11 @@ class CardPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     List<Item> carros = [
-      Item(1, "", "", ""),
-      Item(2, "", "", ""),
-      Item(3, "", "", ""),
-      Item(4, "", "", ""),
-      Item(5, "", "", ""),
+      Item(1, "https://img.volanty.com/thumb/inspecao/5770053978947584/20190823/1566553500485.jpg", "Clio", "Renault Clio 1.0 - 2019"),
+      Item(2, "https://hyundai.itavema.com.br/media/catalog/product/cache/1/thumbnail/9df78eab33525d08d6e5fb8d27136e95/h/b/hb20-1.jpg", "HB20", "Novo HB20 - 2020"),
+      Item(3, "https://www.vw.com.br/content/dam/vw-ngw/vw_pkw/importers/br/models/my19/gol/highlights/Highlight_Gol_16x9_1216x684px_02A.jpg/_jcr_content/renditions/original.transform/max/img.jpg", "Gol", "VW Gol G& - 2019"),
+      Item(4, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTiuBuyib-idfu-s7o1-PRCLG4kCK98GaPk2fM8Mx6FReATNvAD&usqp=CAU", "KA", "Ford KA 1.0 - 2020"),
+      Item(5, "https://http2.mlstatic.com/disco-de-freio-ventilado-fiat-uno-vivace-uno-way-2010-D_NQ_NP_819559-MLB31737233830_082019-F.jpg", "Uno", "FIAT Uno Vivace - 2019"),
     ];
 
     return Container();
